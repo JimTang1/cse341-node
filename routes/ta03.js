574de7782c8c2e751c2d0dc45e0e84e5ad46556e
JimTang1 <after062111@gmail.com>
2021-09-28 20:09:51 -0600

    team activity 03

--- routes/ta03.js
+++ routes/ta03.js
@@ -2,13 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
-  });
-});
+const controller3 = require('../controllers/ta03');
+
+router.get('/', controller3.getAllProds);
+
+router.get('/search', controller3.getSearch);
 
 module.exports = router;
